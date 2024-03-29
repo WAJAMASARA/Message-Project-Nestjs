eec46b2556365eea5954e1ac133de03a69a5b87e
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-03-29 18:25:02 +0530

    use injector module Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-  constructor() {
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
   @Get()
   listMessages() {
     return this.messagesService.findAll();
@@ -16,7 +20,11 @@ export class MessagesController {
     return this.messagesService.create(body.content);
   }
   @Get('/:id')
-  recivemessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async recivemessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
   }
 }
